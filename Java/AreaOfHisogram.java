a717fff2b1831f4552c9cdb165974bad4129e455
Pragya Sharma <67971009+pragya-sharma11@users.noreply.github.com>
2021-10-03 12:00:42 +0530

    create maxAreaOfHistogram function to find the largest area in histogram and return the answer back to main function.

--- Java/AreaOfHisogram.java
+++ Java/AreaOfHisogram.java
@@ -10,4 +10,35 @@ public class AreaOfHistogram{
         }
         System.out.println("Maximum Area of Histogram is " + maxAreaOfHistogram(input));
     }
+    public static int maxAreaOfHistogram(int[] input) {
+        int maxArea = Integer.MIN_VALUE, area = 0, i = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        for (i = 0; i < input.length;) {
+            if (stack.isEmpty() || input[stack.getFirst()] <= input[i]) {
+                stack.offerFirst(i++);
+            } else {
+                int top = stack.pollFirst();
+                if (stack.isEmpty()) {
+                    area = input[top] * i;
+                } else {
+                    area = input[top] * (i - stack.peekFirst() - 1);
+                }
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            int top = stack.pollFirst();
+            if (stack.isEmpty()) {
+                area = input[top] * i;
+            } else {
+                area = input[top] * (i - stack.peekFirst() - 1);
+            }
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+        return maxArea;
+    }
 }
