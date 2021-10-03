c8210b71725da02f6ba8a51aec7ab6e609ce95af
Pragya Sharma <67971009+pragya-sharma11@users.noreply.github.com>
2021-10-03 11:59:10 +0530

    create main function

--- Java/AreaOfHisogram.java
+++ Java/AreaOfHisogram.java
@@ -1,3 +1,13 @@
 public class AreaOfHistogram{
-  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of bars in histogram");
+        int length = scanner.nextInt();
+        int[] input = new int[length];
+        System.out.printf("Enter the elements");
+        for (int i = 0; i < length; i++) {
+            input[i] = scanner.nextInt();
+        }
+        System.out.println("Maximum Area of Histogram is " + maxAreaOfHistogram(input));
+    }
 }
